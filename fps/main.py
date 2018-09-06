eb4699709e8d2576110afcde3eea3dfe2ee2fbf8
Sam Kingston <sam@sjkwi.com.au>
2018-09-06 20:52:19 +1000

    refactor classes and create asteroids on timer

--- fps/main.py
+++ fps/main.py
@@ -11,40 +11,68 @@ resource.reindex()
 WINDOW_WIDTH = 0
 WINDOW_HEIGHT = 0
 
-def wrap(value, limit):
-    if value > limit:
-        value -= limit
-    if value < 0:
-        value += limit
-    return value
-
-class Sprite(pyglet.sprite.Sprite):
+class SpriteOutOfBounds(Exception):
     pass
 
-class MovingSprite(Sprite):
-    dx = 0
-    dy = 0
-    rotation_speed = 0
+class LinkedSprite:
+    """
+    Allows a sprite to have other sprites linked to it, where (x, y) movement
+    is cascaded. This allows sprites to move as a group.
+    """
 
-    def __init__(self, img, x, y):
-        self.radius = int(img.width / 2)
+    linked_sprites = None
+
+    def __init__(self, *args, **kwargs):
         self.linked_sprites = []
-        super().__init__(img, x, y)
-    
-    def update(self, dt):
-        x = int(self.x + self.dx * dt)
-        y = int(self.y + self.dy * dt)
-        self.x = wrap(x, WINDOW_WIDTH)
-        self.y = wrap(y, WINDOW_HEIGHT)
+        super().__init__(*args, **kwargs)
 
+    def link(self, sprite):
+        self.linked_sprites.append(sprite)
+
+    def update(self, dt):
         for sprite in self.linked_sprites:
             sprite.x = self.x + self.radius
             sprite.y = self.y + self.radius
-
+    
     def draw(self):
-        super().draw()
         for sprite in self.linked_sprites:
             sprite.draw()
+        super().draw()
+
+    def delete(self):
+        for sprite in self.linked_sprites:
+            self.linked_sprites.remove(sprite)
+            sprite.delete()
+
+class BaseSprite(LinkedSprite, pyglet.sprite.Sprite):
+    """
+    Provides a base class for sprites.
+    """
+
+    debug_text = 'Update me in subclass'
+    debug_label = None
+
+    def __init__(self, img, x, y):
+        self.radius = int(img.width / 2)
+        img.anchor_x = img.width // 2
+        img.anchor_y = img.height // 2
+        super().__init__(img, x, y)
+
+    def update_debug_label(self):
+        if self.debug_label:
+            self.debug_label.text = self.debug_text
+        else:
+            x = self.x - self.image.anchor_x
+            y = self.y - self.image.anchor_y
+            self.debug_label = pyglet.text.Label(text=self.debug_text, 
+                    x=x, y=y, multiline=True, width=500)
+            self.link(self.debug_label)
+
+class CollidableSprite(BaseSprite):
+    """
+    Provides methods for computing the (x, y) coordinates
+    a sprite occupies.
+    """
 
     def collision_cells(self):
         current_x = int(self.x)
@@ -53,34 +81,44 @@ class MovingSprite(Sprite):
             for x in range(current_x - self.radius, current_x + self.radius + 1):
                 yield (x, y)
 
-class DebugInfoSprite:
-    _debug_label = None
-
-    def update_debug_label(self):
-        if self._debug_label:
-            self._debug_label.text = self.debug_text
-        else:
-            self._debug_label = pyglet.text.Label(text=self.debug_text, 
-                    x=self.x, y=self.y, multiline=True, width=500)
-            self.linked_sprites.append(self._debug_label)
-
-class Asteroid(MovingSprite, DebugInfoSprite):
-    MAX_ROTATION_SPEED = 20.0
-
-    def __init__(self, img, x, y):
-        super().__init__(img, x, y)
-        self.dx = (random.random() - 0.5) * 25
-        self.dy = (random.random() - 0.5) * 25
-        self.rotation = random.random() * 360.0
-        self.rotation_speed = random.random() * self.MAX_ROTATION_SPEED
+class MovingSprite(BaseSprite):
+    dx = 0
+    dy = 0
+    
+    def update(self, dt):
+        self.x = int(self.x + self.dx * dt)
+        self.y = int(self.y + self.dy * dt)
+        if hasattr(self, 'rotation_speed'):
+            self.rotation += (self.rotation_speed * dt)
+        super().update(dt)
+        self.check_bounds()
 
     @property
     def debug_text(self):
         s = self.__class__.__name__ + '\n'
         s += f'({self.x}, {self.y})\n'
-        s += f'dx={int(self.dx)} dy={int(self.dy)}\n'
-        s += f'r={self.rotation}\n'
+        dx = int(self.dx)
+        dy = int(self.dy)
+        rot = int(self.rotation)
+        s += f'dx={dx} dy={dy} r={rot}\n'
         return s
+
+class Asteroid(MovingSprite, CollidableSprite, BaseSprite):
+    max_rotation_speed = 10.0
+
+    def __init__(self, img, x, y):
+        super().__init__(img, x, y)
+
+        self.dx = -random.randint(10, 40)
+        self.rotation = random.random() * 360.0
+        self.rotation_speed = random.random() * self.max_rotation_speed
+
+    def check_bounds(self):
+        """
+        Asteroids should be deleted when they leave the viewport on the left side.
+        """
+        if self.x <= 0:
+            raise SpriteOutOfBounds()
 
 class StarImage:
     def __init__(self, img, x, y):
@@ -120,52 +158,50 @@ class StarImageField:
         for i in self.imgs:
             i.draw()
 
-class Ship(MovingSprite, DebugInfoSprite):
-    ROTATION_SPEED = 180.0
+class Ship(MovingSprite, CollidableSprite, BaseSprite):
+    max_speed = 200.0
 
     def __init__(self, img):
-        # centre anchor
-        img.anchor_x = img.width //2
-        img.anchor_y = img.height // 2
-
-        initial_x = WINDOW_WIDTH // 2
+        initial_x = WINDOW_WIDTH // 6
         initial_y = WINDOW_HEIGHT // 2
 
-        self.key_handler = key.KeyStateHandler()
-        
         super().__init__(img, initial_x, initial_y)
 
+        self.rotation = 90
+        self.key_handler = key.KeyStateHandler()
+
     def update(self, dt):
-        if self.key_handler[key.LEFT]:
-            self.rotation -= self.ROTATION_SPEED * dt
-        if self.key_handler[key.RIGHT]:
-            self.rotation += self.ROTATION_SPEED * dt
-
-        angle_radians = -math.radians(self.rotation)
-        force_x = -math.sin(angle_radians) * 200.0 * dt
-        force_y = math.cos(angle_radians) * 200.0 * dt
-
         if self.key_handler[key.UP]:
-            self.dx += force_x
-            self.dy += force_y
+            self.dy += self.max_speed * dt
 
-        if self._debug_label:
+        if self.key_handler[key.DOWN]:
+            self.dy -= self.max_speed * dt
+
+        if self.key_handler[key.LEFT]:
+            self.dx -= self.max_speed * dt
+
+        if self.key_handler[key.RIGHT]:
+            self.dx += self.max_speed * dt
+
+        if self.debug_label:
             self.update_debug_label()
 
         super().update(dt)
 
-    @property
-    def debug_text(self):
-        s = self.__class__.__name__ + '\n'
-        s += f'({self.x}, {self.y})\n'
-        s += f'dx={self.dx} dy={self.dy} r={self.rotation}\n'
-        return s
+    def check_bounds(self):
+        """
+        Ships should be blocked from going out of the viewport
+        by stopping their velocity.
+        """
+        if self.x > WINDOW_WIDTH or self.x < 0:
+            self.dx = 0
+        if self.y > WINDOW_HEIGHT or self.y < 0:
+            self.dy = 0
 
 class Game(pyglet.window.Window):
     sprites = []
 
     def __init__(self, *args, **kwargs):
-        num_asteroids = kwargs.pop('asteroids', 10)
         self.debug = kwargs.pop('debug', False)
 
         # super() must be called before setting dimmensions
@@ -180,9 +216,9 @@ class Game(pyglet.window.Window):
         self.fps.label = pyglet.text.Label(y=WINDOW_HEIGHT-20)
 
         # set up sprites
-        self.sprites.append(Sprite(resource.image('white.png'), 0, 0))
+        self.sprites.append(pyglet.sprite.Sprite(resource.image('white.png'), 0, 0))
         self.sprites.append(StarImageField(resource.image('star.jpg')))
-        self.sprites.extend(self.get_asteroids(num_asteroids))
+        #self.sprites.extend(self.get_asteroids(random.randint(1, 7)))
 
         # player ship
         self.ship = Ship(resource.image('ship.png'))
@@ -190,6 +226,7 @@ class Game(pyglet.window.Window):
         self.sprites.append(self.ship)
 
         pyglet.clock.schedule_interval(self.update, 1/60.0)
+        pyglet.clock.schedule_once(self.new_asteroids, 1)
 
     def on_draw(self):
         self.clear()
@@ -208,23 +245,26 @@ class Game(pyglet.window.Window):
     def on_mouse_press(self, x, y, button, modifiers):
         if button == mouse.LEFT:
             obj = self.check_collisions().get(x+y, None)
-            if obj:
-                obj.update_debug_label()
+            print('clicked', obj)
 
     def update(self, dt):
         for sprite in self.sprites:
             if hasattr(sprite, 'update'):
-                sprite.update(dt)
+                try:
+                    sprite.update(dt)
+                except SpriteOutOfBounds:
+                    sprite.delete()
+                    self.sprites.remove(sprite)
+                    print('Deleted', sprite, 'at', sprite.x, sprite.y)
 
     def get_asteroids(self, num):
-        grid = pyglet.image.ImageGrid(resource.image('asteroids.png'), 8, 8)
         asteroids = []
+        grid = pyglet.image.ImageGrid(resource.image('asteroids.png'), 8, 8)
         for i in range(num):
-            x = random.randint(0, self.width)
-            y = random.randint(0, self.height)
-            index = (random.randint(0, 7), random.randint(0, 7))
-            asteroid = Asteroid(img=grid[index], x=x, y=y)
-            asteroid.rotation = random.randint(0, 360)
+            img = grid[(random.randint(0, 7), random.randint(0, 7))]
+            x = self.width + (img.width * 2)
+            y = random.randint(img.height, self.height-img.height)
+            asteroid = Asteroid(img=img, x=x, y=y)
             asteroids.append(asteroid)
         return asteroids
 
@@ -235,6 +275,12 @@ class Game(pyglet.window.Window):
                 for x, y in sprite.collision_cells():
                     hits[x+y] = sprite
         return hits
+
+    def new_asteroids(self, dt):
+        num = random.randint(0, 2)
+        self.sprites.extend(self.get_asteroids(num))
+        pyglet.clock.schedule_once(self.new_asteroids, random.randint(4, 6))
+
 
 def main():
     window = Game(
